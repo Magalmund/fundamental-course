1a22d71b0d39d93c338e15c798e8d268cc184245
Nikita <poker6-96@mail.com>
2022-07-31 23:39:16 +0300

    'change'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import React from 'react'
+import './styles/App.css'
 
 function App() {
   return (
     <div className="App">
-			
+			<div className="post">
+				<div className="post-content">
+					<strong>1. JavaScript</strong>
+					<div>
+						JavaScript - язык программирования
+					</div>
+				</div>
+				<div className="post-btns">
+					<button>Удаление</button>
+				</div>
+			</div>
     </div>
   );
 }
