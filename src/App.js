301b0b7ce9f9c001542184617a353a24cc97ae36
Kaloid <poker6-96@mail.ru>
2022-08-20 10:03:50 +0300

    change

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import Modal from './components/UI/Modal/Modal';
 import {usePosts} from "./hooks/usePosts"
 import './styles/App.css'
 import PostService from './API/PostService';
-import Loader from './components/UI/Loader/Loader'
+import Loader from './components/UI/Loader/Loader';
+import {useFetching} from './hooks/useFetching';
 
 function App() {
 	const [posts, setPosts] = useState([])
 	const [filter, setFilter] = useState({sort:'', query:''})
 	const [modal, setModal] = useState(false);
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-	const [isPostsLoading, setIsPostsLoading] = useState(false);
+	const [fetchPosts, isPostsLoading, postError] = useFetching(async() => {
+		const posts = await PostService.getAll();
+		setPosts(posts)
+	})
 
 	useEffect(() => {
 		fetchPosts()
@@ -25,26 +29,21 @@ function App() {
 		setModal(false)
 	}
 
-	async function fetchPosts() {
-		setIsPostsLoading(true);
-		const posts = await PostService.getAll();
-		setPosts(posts);
-		setIsPostsLoading(false);
-	}
-
 	function removePost (post) {
 		setPosts(posts.filter(p => p.id !== post.id))
 	}
 
   return (
     <div className="App">
-			<Button onClick={fetchPosts}>Get posts</Button>
-			<Button style={{marginTop:30}} onClick={() => setModal(true)}>Создать пользователя</Button>
+			<Button style={{marginTop:30}} onClick={() => setModal(true)}>Создать пост</Button>
 			<Modal visible={modal} setVisible={setModal}>
 				<PostForm create={createPost}/>
 			</Modal>
 			<hr style={{margin:'15px 0'}}/>
 			<PostFilter filter={filter} setFilter={setFilter}/>
+			{postError &&
+				<h1>Произошла ошибка ${postError}</h1>
+			}
 			{isPostsLoading
 				? <div style={{display: 'flex', justifyContent: 'center', marginTop:50}}><Loader/></div>
 				: <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Список постов'}/>
